f9a247231be40bf63f0bab1f6688233518b50965
Ravaldini <pravaldini@gmail.com>
2018-02-04 20:33:00 +0300

    Initial commit

---
+++ Assets/Scripts/Water_flip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water_flip : MonoBehaviour {
+
+	public GameObject player;
+	private Transform tr;
+
+	// Use this for initialization
+	void Start () {		
+
+		tr = GetComponent<Transform>();
+		Debug.Log(tr.transform.position.x);
+
+		tr.transform.position.Set (1000f, 0f, 0f);
+		Debug.Log(tr.transform.position.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (player.transform.position.y <= 49.1) {
+			//Debug.Log ("Under water now!");
+		}
+
+		if (player.transform.position.y > 49.1) {
+			//Debug.Log ("Back on air!");
+		}
+
+	}
+}
