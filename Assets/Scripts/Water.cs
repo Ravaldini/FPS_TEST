904c922b6510002ec4d8dfff5a0ea814aaa37f08
Ravaldini <pravaldini@gmail.com>
2018-02-11 13:56:04 +0300

    Скайбокс

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (player.transform.position.y <= 49.2) {
+			Debug.Log ("Under water now!");
+			System.
+		}
+
+		if (player.transform.position.y > 49.2) {
+			Debug.Log ("Back on air!");
+		}
+	}
+}
